43f61b7da316fc7b28649920547cfb93c14c7ee7
Maxim Melnik <mm@albedo.dev>
2019-11-11 17:54:29 +0200

    Very basic router setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+// 2. Определяем несколько маршрутов
+// Каждый маршрут должен указывать на компонент.
+// "Компонентом" может быть как конструктор компонента, созданный
+// через `Vue.extend()`, так и просто объект с опциями компонента.
+// Мы поговорим о вложенных маршрутах позднее.
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+// 3. Создаём экземпляр маршрутизатора и передаём маршруты в опции `routes`
+// Вы можете передавать и дополнительные опции, но пока не будем усложнять.
+const router = new VueRouter({
+  routes // сокращённая запись для `routes: routes`
 })
+
+// 4. Создаём и монтируем корневой экземпляр приложения.
+// Убедитесь, что передали экземпляр маршрутизатора в опции
+// `router`, чтобы позволить приложению знать о его наличии.
+const app = new Vue({
+  router
+}).$mount('#app')
+
